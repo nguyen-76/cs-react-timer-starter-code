20c766f793e4d2bf0fd62db1e36d23978551bb6f
Nguyen Doan <nguyendoan3456@gmail.com>
2023-12-19 22:25:38 -0500

    add $ infront of const minutes

--- src/formatTime.js
+++ src/formatTime.js
@@ -1,6 +1,6 @@
 export const formatTime = (time) => {
   const getSeconds = `0${time % 60}`.slice(-2);
-  const minutes = `{Math.floor(time / 60)}`;
+  const minutes = `${Math.floor(time / 60)}`;
   const getMinutes = `0${minutes % 60}`.slice(-2);
   const getHours = `0${Math.floor(time / 3600)}`.slice(-2);;
 
